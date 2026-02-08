1af76ddaa3989f19fa39405fdd8efefcafc80aa3
SahilIjaz <hssahil2913@gmail.com>
2026-02-08 23:19:17 +0500

    frontend started

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600"> Omni-Agent Dashboard</h1>
+    </main>
+  );
+}
